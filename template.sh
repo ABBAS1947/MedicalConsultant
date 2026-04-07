ba732d226fe01f85415b6ce8e89a2e51f8f3f28b
Meesam <meesam1947@gmail.com>
2026-04-07 11:42:44 +0400

    added structure for the project

--- template.sh
+++ template.sh
@@ -1,16 +1,26 @@
-# Creating directory 
-mkdir -p src 
-mkdir -p research 
+mkdir -p src/components
+mkdir -p src/utils
+mkdir -p data/raw
+mkdir -p data/processed
+mkdir -p vectorstore
+mkdir -p research
 
-# Creating files
-touch src/helper.py 
-touch src/prompt.py
+touch src/__init__.py
+touch src/components/loader.py
+touch src/components/splitter.py
+touch src/components/embedding.py
+touch src/components/retriever.py
+touch src/components/rag_chain.py
+
+touch src/utils/helper.py
+touch src/utils/prompt.py
+
+touch app.py
+touch config.py
 touch .env
-touch setup.py 
-touch app.py 
-touch research/trials.ipynb
+touch setup.py
 touch requirements.txt
 
+touch research/trials.ipynb
 
-
-echo "Done"
+echo "Structure Ready"
